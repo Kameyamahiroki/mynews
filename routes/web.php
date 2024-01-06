b30e6404cbb6d2e1e7bdebd06a4569c56bcdbcb2
亀山大樹 <h-kameyama081@jutjoy.com>
2024-01-06 18:29:47 +0900

    課題を更新します

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::controller(NewsController::class)->prefix('admin')->name('news.')->group(function(){
+    Route::get('news/create', 'add')->name('add');
+});
+
+Route::prefix('admin')->middleware('auth')->name('profile.')->group(function () {
+    Route::get('profile/create', 'Admin\SelfProfileController@add')->name('add');
+    Route::post('profile/create', 'Admin\SelfProfileController@create')->name('create');
+    Route::get('profile/edit', 'Admin\SelfProfileController@edit')->name('edit');
+    Route::post('profile/edit', 'Admin\SelfProfileController@update')->name('update');
+    Route::get('profile', 'Admin\SelfProfileController@index')->name('index');
+    Route::get('profile/delete', 'Admin\SelfProfileController@delete')->name('delete');
+});
